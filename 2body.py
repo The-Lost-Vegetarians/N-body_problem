9b12fd2ccd47d0133af8169f881d860aacb0632a
deep-lodaya <deep.lodaya05@gmail.com>
2025-08-29 13:29:12 +0530

    Update 2body.py

--- 2body.py
+++ 2body.py
@@ -12,3 +12,14 @@ def rk4_step(f, t, y, h):
     k4 = f(t + h,   y + h * k3)
     return y + (h/6) * (k1 + 2*k2 + 2*k3 + k4)
 
+# -----------------------------------------
+# 2. Two-body ODEs (relative COM frame)
+# State: [x, y, vx, vy]
+# -----------------------------------------
+def two_body(t, y, G=4*np.pi**2, M=1.0):
+    x, y_pos, vx, vy = y
+    r = np.sqrt(x**2 + y_pos**2)
+    ax = -G * M * x / r**3
+    ay = -G * M * y_pos / r**3
+    return np.array([vx, vy, ax, ay])
+
